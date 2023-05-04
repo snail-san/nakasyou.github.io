973bb6ab494270ae2793d1abf28e6b03a90970ff
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-04 20:54:12 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/styles/tailwind.css"],
-    build: {
-	        postcss: {
-			      postcssOptions: {
-				              plugins: {
-						                tailwindcss: {},
-								        },
-									      },
-									          },
-										    },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+        },
+      },
+    },
+  },
 })
