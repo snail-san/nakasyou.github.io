4744773dcb06afc38e896cf2531bcfdb901ce223
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-04 20:52:17 +0900

    format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-	  content: [
-		      "app.vue",
-		      "./components/**/*.{vue,js,jsx,ts,tsx}",
-		      "./pages/**/*.{vue,js,jsx,ts,tsx}",
-		    ],
-	  theme: {
-		      extend: {},
-		    },
-	  plugins: [],
+  content: [
+    "app.vue",
+    "./components/**/*.{vue,js,jsx,ts,tsx}",
+    "./pages/**/*.{vue,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
