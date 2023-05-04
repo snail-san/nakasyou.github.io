5f94cca6ae8a6b9a145a93ba7f310480985e4dce
nakasyou <nakasyou1103@gmail.com>
2023-05-04 12:05:28 +0000

    Add daisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss",],
+  buildModules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/styles/tailwind.css"],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-        },
-      },
-    },
-  },
 })
