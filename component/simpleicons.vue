9cf5e3065c59f3bd7734b21a0f56185446ddcf09
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-05 18:50:17 +0900

    Create simpleicons.vue

---
+++ component/simpleicons.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class: string
+  icon: string
+}>()
+</script>
+<template>
+  <img :class="class" :src="`https://cdn.jsdelivr.net/npm/simple-icons@8.12.1/icons/${icon}.svg`" />
+</template>
