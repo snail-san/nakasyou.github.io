8e42bb468acb021f7d27d26429d2f493da4d1835
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-05 10:28:30 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
+<script setup type="ts">
+
+</script>
 <template>
   <div>
-    <h1 class=" pl-24 pt-12 text-3xl text-red-600 underline">nakasyou</h1>
+    <h1 class="text-center">nakasyou's profile</h1>
+    <div>
+      <img alt="nakasyou-icon" href="https://github.com/nakasyou.png">
+      <div>Shotaro Nakamura</div>
+    </div>
   </div>
 </template>
-<script>
-</script>
