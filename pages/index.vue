7cdb6dafce0060863f9c74912118e78732afcf29
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-05 12:11:32 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,21 @@
 </script>
 <template>
   <div>
-    <h1 class="text-center">nakasyou's profile</h1>
-    <div class="flex">
+    <div class="text-center text-3xl">nakasyou's profile</div>
+    <div class="flex text-center">
       <img
         alt="nakasyou-icon"
         src="~/assets/nakasyou-icon.png"
-        class="rounded-full w-6 h-6" />
-      <div>Shotaro Nakamura</div>
+        class="rounded-full w-24 h-24" />
+      <div class="ext-xl">Shotaro Nakamura</div>
+    </div>
+    <div>About</div>
+    <div>
+      <div>
+        <div>Shotaro Nakamura</div>
+        <div>中村 承太郎</div>
+      </div>
+      <div>Web開発とブロックチェーン、サイバーセキュリティに興味がある中学2年生</div>
     </div>
   </div>
 </template>
