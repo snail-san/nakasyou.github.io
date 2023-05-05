9db09247ee51cbb4c5bf7bc2d2176c8edc1697c6
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-05 10:32:49 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
 <template>
   <div>
     <h1 class="text-center">nakasyou's profile</h1>
-    <div>
-      <img alt="nakasyou-icon" href="https://github.com/nakasyou.png">
+    <div class="flex">
+      <img alt="nakasyou-icon" href="https://avatars.githubusercontent.com/u/79000684?v=4">
       <div>Shotaro Nakamura</div>
     </div>
   </div>
