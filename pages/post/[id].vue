0634356a305b63856b8938364f75fb76ae463c28
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 11:59:09 +0900

    add default font-size

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -6,3 +6,23 @@
     </ContentDoc>
   </div>
 </template>
+<style scoped>
+h1 {
+  font-size: 2em;
+}
+h2 {
+  font-size: 1.5em;
+}
+h3 {
+  font-size: 1.17em;
+}
+h4 {
+  font-size: 1.00em;
+}
+h5 {
+  font-size: 0.83em;
+}
+h6 {
+  font-size: 0.67em;
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -10,3 +10,23 @@
     </ContentList>
   </div>
 </template>
+<style scoped>
+h1 {
+  font-size: 2em;
+}
+h2 {
+  font-size: 1.5em;
+}
+h3 {
+  font-size: 1.17em;
+}
+h4 {
+  font-size: 1.00em;
+}
+h5 {
+  font-size: 0.83em;
+}
+h6 {
+  font-size: 0.67em;
+}
+</style>
