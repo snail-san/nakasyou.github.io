5498563b60f082d5c559acf879201ba04e3e8e42
nakasyou <nakasyou1103@gmail.com>
2023-05-06 02:35:50 +0000

    Add @nuxt/content

---
+++ pages/post/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <ContentDoc v-slot="{ doc: page }">
+      <h1>{{ page.title }}</h1>
+      <ContentRenderer :key="page._id" :value="page"></ContentRenderer>
+    </ContentDoc>
+  </div>
+</template>
---
+++ pages/post/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ContentList v-slot="{ list }" :query="{ path: '/post', sort: { date: -1 } }">
+      <article v-for="page in list" :key="page._path">
+        <nuxt-link :to="page._path">
+          <h2>{{ page.title }}</h2>
+        </nuxt-link>
+        <ContentDoc :path="page._path" excerpt :head="false"></ContentDoc>
+      </article>
+    </ContentList>
+  </div>
+</template>
