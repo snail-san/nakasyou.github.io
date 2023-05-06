df35e415ac0d6bfb337ec52013d8cf1d7d7a8b60
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 15:17:02 +0900

    test

--- components/header.vue
+++ components/header.vue
@@ -17,7 +17,7 @@
         </div> 
         <div class="drawer-side fixed">
           <label for="my-drawer" class="drawer-overlay"></label>
-          <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+          <ul class="menu bg-base-100 text-base-content">
             <li><a>Sidebar Item 1</a></li>
             <li><a>Sidebar Item 2</a></li>         
           </ul>
