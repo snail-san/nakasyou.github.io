297f7011ed6dcd13ec341a5e18d498cb25065432
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 15:12:53 +0900

    fixed

--- components/header.vue
+++ components/header.vue
@@ -15,7 +15,7 @@
         <div class="drawer-content">
           <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
         </div> 
-        <div class="drawer-side">
+        <div class="drawer-side fixed">
           <label for="my-drawer" class="drawer-overlay"></label>
           <ul class="menu p-4 w-80 bg-base-100 text-base-content">
             <li><a>Sidebar Item 1</a></li>
