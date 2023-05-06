4f9ee00af91149ea8fce24bf3f9f2275a3b1f36f
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 11:44:34 +0900

    add no-image

--- components/min-works.vue
+++ components/min-works.vue
@@ -6,7 +6,7 @@ import works from "./works.ts"
     <div v-for = "work in (works.slice(0,10).reverse())" :key="work.link" class="carousel-item mx-1">
       <a :href="work.link">
         <div class="card w-40 bg-base-100 shadow-xl image-full">
-          <figure><img :src="work.imageUrl? work.imageUrl : ''" :alt="work.title" /></figure>
+          <figure><img :src="work.imageUrl? work.imageUrl : '/work-images/no-image.png'" :alt="work.title" /></figure>
           <div class="card-body">
             <div class="card-title">
               {{ work.title }}
