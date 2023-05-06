59ddb1919f1a8915c3b2dbc54671def8570f553d
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 14:43:22 +0900

    mmax-lg

--- components/header.vue
+++ components/header.vue
@@ -5,13 +5,11 @@
     <div class="navbar-start">
       <NuxtLink class="btn btn-ghost normal-case text-xl" href="/">nakasyou's website</NuxtLink>
     </div>
-    <div class="navbar-center">
-      <ul class="menu menu-horizontal bg-base-100 rounded-box">
+    <div class="navbar-end">
+      <ul class="menu menu-horizontal bg-base-100 rounded-box max-lg:hidden">
         <li><NuxtLink to="/post">Blog</NuxtLink></li>
         <li><NuxtLink to="/works">Works</NuxtLink></li>
       </ul>
-    </div>
-    <div class="navbar-end">
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
