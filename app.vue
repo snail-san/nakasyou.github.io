33ae5bb6c14c4ffbf4244801078c58bd61dc2b54
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-07 08:44:03 +0900

    GTag

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 onMounted(() => {
-  setInterval(()=>console.log('mounted'),100)
+  {
+    const script = document.createElement("script")
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-X7S9B5WF1H"
+    script.async= true
+    document.head.append(script)
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-X7S9B5WF1H');
+  }
 });
 </script>
 <template>
