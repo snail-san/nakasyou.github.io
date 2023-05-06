7c57d1e2c36b2e638cf482eb4c746a163ce799a9
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 15:00:23 +0900

    drawerに変更

--- components/header.vue
+++ components/header.vue
@@ -10,14 +10,20 @@
         <li><NuxtLink to="/post">Blog</NuxtLink></li>
         <li><NuxtLink to="/works">Works</NuxtLink></li>
       </ul>
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
-        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-          <li><NuxtLink to="/post">Blog</NuxtLink></li>
-          <li><NuxtLink to="/works">Works</NuxtLink></li>
-        </ul>
+      <div class="drawer">
+        <input id="header-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+          <label tabindex="0" class="btn btn-ghost lg:hidden drawer-button" for="header-drawer">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+        </div>
+        <div class="drawer-side">
+          <label for="header-drawer" class="drawer-overlay"></label>
+          <ul tabindex="0" class="menu p-4 w-80 bg-base-100 text-base-content">
+            <li><NuxtLink to="/post">Blog</NuxtLink></li>
+            <li><NuxtLink to="/works">Works</NuxtLink></li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
