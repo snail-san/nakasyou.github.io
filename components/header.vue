a082c71ea62441727a9856c03335edafed9dc55b
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 15:19:30 +0900

    fix

--- components/header.vue
+++ components/header.vue
@@ -10,19 +10,19 @@
         <li><NuxtLink to="/post">Blog</NuxtLink></li>
         <li><NuxtLink to="/works">Works</NuxtLink></li>
       </ul>
-      <div class="drawer">
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer drawer-end">
+        <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content">
-          <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+          <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
         </div> 
-        <div class="drawer-side fixed">
-          <label for="my-drawer" class="drawer-overlay"></label>
-          <ul class="menu bg-base-100 text-base-content">
+        <div class="drawer-side">
+          <label for="my-drawer-4" class="drawer-overlay"></label>
+          <ul class="menu p-4 w-80 bg-base-100 text-base-content">
             <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>         
+            <li><a>Sidebar Item 2</a></li>
           </ul>
         </div>
-      </div>
+      </div>    
     </div>
   </div>
 </template>
