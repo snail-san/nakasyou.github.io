5d16c0cebb0c398f0b0fe0bb606bb1cfcb90a42e
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 15:42:52 +0900

    fix

--- components/header.vue
+++ components/header.vue
@@ -10,19 +10,15 @@
         <li><NuxtLink to="/post">Blog</NuxtLink></li>
         <li><NuxtLink to="/works">Works</NuxtLink></li>
       </ul>
-      <div class="drawer drawer-end">
-        <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content">
-          <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
-        </div> 
-        <div class="drawer-side">
-          <label for="my-drawer-4" class="drawer-overlay"></label>
-          <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-          </ul>
-        </div>
-      </div>    
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+          <li><NuxtLink to="/post">Blog</NuxtLink></li>
+          <li><NuxtLink to="/works">Works</NuxtLink></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
