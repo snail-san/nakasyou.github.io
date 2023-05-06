f180f99cd6c24820ee61c0854b74642cbaa6cc98
Shotaro N <79000684+nakasyou@users.noreply.github.com>
2023-05-06 11:00:35 +0900

    debt-clockという作品の追加

--- components/works.ts
+++ components/works.ts
@@ -11,6 +11,12 @@ const works: Works = [
     link: "https://liberluna.github.io",
     desc: "Liberlunaのサイトを作りました",
     imageUrl: "/work-images/liberluna-page.jpeg",
+  },
+  {
+    title: "借金時計",
+    link: "https://nakasyou.github.io/debt-clock",
+    desc: "OSSの借金時計がなかったので、作ってみました。",
+    imageUrl: "/work-images/debt-clock.jpeg",
   }
 ]
 
