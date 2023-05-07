0552a154065705d6ca325593e05ddbfc50164564
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 14:47:04 +0900

    非⭐️破壊

--- components/min-works.vue
+++ components/min-works.vue
@@ -3,7 +3,7 @@ import works from "./works.ts"
 </script>
 <template>
   <div class="carousel rounded-box">
-    <div v-for = "work in (works.slice(0,10).reverse())" :key="work.link" class="carousel-item mx-1">
+    <div v-for = "work in ([...works].slice(0,10).reverse())" :key="work.link" class="carousel-item mx-1">
       <a :href="work.link">
         <div class="card w-52 bg-base-100 shadow-xl image-full">
           <figure><img :src="work.imageUrl? work.imageUrl : '/work-images/no-image.png'" :alt="work.title" /></figure>
