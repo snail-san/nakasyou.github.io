04c1b80cd7147f4e19c2290c1e9f5f28f050dad1
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 13:42:34 +0900

    Create work.vue

---
+++ components/works/work.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { type Work } from "../works.ts"
+
+const props = defineProps<
+  work: Work
+>()
+</script>
+<template>
+  <a :href="work.link">
+    <div class="card w-52 bg-base-100 shadow-xl image-full">
+      <figure><img :src="work.imageUrl? work.imageUrl : '/work-images/no-image.png'" :alt="work.title" /></figure>
+        <div class="card-body">
+          <div class="card-title break-all">
+            {{ work.title }}
+          </div>
+        <p class="break-all">{{ work.desc }}</p>
+      </div>
+    </div>
+  </a>
+</template>
