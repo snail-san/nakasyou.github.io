00bb086ad31a7f1367ab79fe5ee1ceae813a0475
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 13:59:48 +0900

    Update index.vue

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -5,6 +5,12 @@ import works from "../../components/works.ts"
 </script>
 <template>
   <header><Header /></header>
-  <h1 class="text-3xl">Works</h1>
-  <Work :work="works[0]" />
+  <div class="mx-10">
+    <h1 class="text-3xl">Works</h1>
+    <div class="grid grid-cols-4 gap-4"
+      <div v-for = "work in works.reverse()" :key="work.link" class="mx-1">
+        <Work :work="work" />
+      </div>
+    </div>
+  </div>
 </template>
