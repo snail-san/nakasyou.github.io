deb34fad5d8c94f5e91c1bfda4655400dd2f55ae
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 14:15:20 +0900

    ヘッダーがないじゃない...

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,6 +3,7 @@ import Header from "../components/header.vue"
 </script>
 
 <template>
+  <header><Header /> </header>
   <div class="mx-10">
     <h1 class="text-xl">nakasyou's Projects!</h1>
     <p>便利で使える、Webツールです。</p>
