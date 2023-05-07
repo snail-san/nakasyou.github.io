f1168afa1c1e5ab289b349f3cb75b4a544e4b30d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 13:45:10 +0900

    Update index.vue

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import Header from "../../components/header.vue"
+import Work from "../../components/works/work.vue"
+import works from "../../components/works.ts"
+</script>
 <template>
+  <header><Header /></header>
   <h1 class="text-3xl">Works</h1>
+  <Work :work="works[0]" />
 </template>
