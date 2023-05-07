0f2e96b2e84f7ed3ec4ae100740ab1d09678a82a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 15:05:39 +0900

    Create [tag].vue

---
+++ pages/post/tag/[tag].vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+const { data } = await queryContent('/post').where({ tags: { $contains: $route.params.tag } })
+</script>
+<template>
+  <div>{{ JSON.stringify(data) }}</div>
+</template>
