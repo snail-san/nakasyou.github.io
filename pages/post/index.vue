416fa86355f3fd32ad9fabbbf105e84dac42533d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-21 20:22:56 +0900

    Fix

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -7,18 +7,19 @@ import Header from "../../components/header.vue"
     <div class="mx-10">
       <h2>Posts</h2>
       <p>nakasyouのブログです。</p>
-      <ContentList class="grid grid-cols-2 gap-4" v-slot="{ list }" :query="{ path: '/post', sort: { date: -1 } }">
-        <article v-for="page in list" :key="page._path">
-          <nuxt-link :to="page._path">
-            <div class="card bg-base-100 shadow-xl m-1">
-              <figure><img :src="page.thumbnail" alt="thumbnail" /></figure>
-              <div class="card-body">
-                <div class="card-title">{{ page.title }}</div>
-                <p>{{ page.desc }}</p>
+      <div class="grid grid-cols-2 gap-4">
+        <ContentList class="" v-slot="{ list }" :query="{ path: '/post', sort: { date: -1 } }">
+          <article v-for="page in list" :key="page._path">
+            <nuxt-link :to="page._path">
+              <div class="card bg-base-100 shadow-xl m-1">
+                <figure><img :src="page.thumbnail" alt="thumbnail" /></figure>
+                <div class="card-body">
+                  <div class="card-title">{{ page.title }}</div>
+                  <p>{{ page.desc }}</p>
+                </div>
               </div>
-            </div>
-          </nuxt-link>
-        </article>
+            </nuxt-link>
+          </article>
       </ContentList>
     </div>
   </div>
