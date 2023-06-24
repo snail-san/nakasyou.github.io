3e45895c4119babb6b7e3c559cf0b6d6ded7fe38
nakasyou <syoutarou1103@outlook.jp>
2023-06-24 18:45:52 +0900

    NuxtJS to Astro

--- components/simpleicons.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const props = defineProps<{
-  class: string
-  icon: string
-}>()
-</script>
-<template>
-  <img :class="class" :src="`https://cdn.jsdelivr.net/npm/simple-icons@8.12.1/icons/${icon}.svg`" />
-</template>
---
+++ data/tags.ts
@@ -0,0 +1,12 @@
+export const tags: Record<number,string> = {
+  0: "Deno",
+  1: "Scratch",
+  2: "JavaScript",
+  3: "Vite",
+  4: "運動会",
+  5: "学校",
+  6: "日記",
+  7: "Vue.js",
+  8: "FastAPI",
+  9: "Pyhton",
+}
