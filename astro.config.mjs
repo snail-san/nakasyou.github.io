878e7f1e1a1f22100e123b4f7371830ac376eb92
nakasyou <syoutarou1103@outlook.jp>
2023-06-26 18:03:07 +0900

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,7 @@ export default defineConfig({
     vue(),
     tailwind(),
     mdx(),
-  ]
+    sitemap(),
+  ],
+  site: 'https://nakasyou.github.io',
 });
